f499a3919a0a49e95607afc29cfda2ac07e2f8dc
Erwan d'Orgeville <info@erwandorgeville.com>
2020-06-06 21:41:47 -0400

    Cleanup

--- app/uibuilder/quiz/src/index.js
+++ app/uibuilder/quiz/src/index.js
@@ -1,21 +1,6 @@
 /* jshint browser: true, esversion: 5, asi: true */
 /* globals Vue, uibuilder */
 // @ts-nocheck
-/*
-  Copyright (c) 2019 Julian Knight (Totally Information)
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
 'use strict'
 
 /** @see https://github.com/TotallyInformation/node-red-contrib-uibuilder/wiki/Front-End-Library---available-properties-and-methods */
@@ -24,14 +9,6 @@
 var app1 = new Vue({
   el: '#app',
   data: {
-    startMsg: 'Vue has started, waiting for messages',
-    feVersion: '',
-    counterBtn: 0,
-    inputText: null,
-    inputChkBox: false,
-    socketConnectedState: false,
-    serverTimeOffset: '[unknown]',
-    imgProps: { width: 75, height: 75 },
     question: 'No question yet',
     questionIndex: null,
     answer: 'radio1',
@@ -42,42 +19,11 @@ var app1 = new Vue({
       // { text: 'Radio 4', value: 'radio4' }
     ],
     timeMessage: 'Standby',
-    timeleft: 0,
-
-    msgRecvd: '[Nothing]',
-    msgsReceived: 0,
-    msgCtrl: '[Nothing]',
-    msgsControl: 0,
-
-    msgSent: '[Nothing]',
-    msgsSent: 0,
-    msgCtrlSent: '[Nothing]',
-    msgsCtrlSent: 0
-  }, // --- End of data --- //
+    timeleft: 0
+  },
   computed: {
     questionLabel: function () {
       return `Question ${this.questionIndex}: ${this.question}`
-    },
-    hLastRcvd: function () {
-      var msgRecvd = this.msgRecvd
-      if (typeof msgRecvd === 'string') return 'Last Message Received = ' + msgRecvd
-      else return 'Last Message Received = ' + this.syntaxHighlight(msgRecvd)
-    },
-    hLastSent: function () {
-      var msgSent = this.msgSent
-      if (typeof msgSent === 'string') return 'Last Message Sent = ' + msgSent
-      else return 'Last Message Sent = ' + this.syntaxHighlight(msgSent)
-    },
-    hLastCtrlRcvd: function () {
-      var msgCtrl = this.msgCtrl
-      if (typeof msgCtrl === 'string') return 'Last Control Message Received = ' + msgCtrl
-      else return 'Last Control Message Received = ' + this.syntaxHighlight(msgCtrl)
-    },
-    hLastCtrlSent: function () {
-      var msgCtrlSent = this.msgCtrlSent
-      if (typeof msgCtrlSent === 'string') return 'Last Control Message Sent = ' + msgCtrlSent
-      // else return 'Last Message Sent = ' + this.callMethod('syntaxHighlight', [msgCtrlSent])
-      else return 'Last Control Message Sent = ' + this.syntaxHighlight(msgCtrlSent)
     },
     sessionId: function () {
       function makeId (length) {
@@ -113,46 +59,8 @@ var app1 = new Vue({
           answer: this.answer
         }
       })
-    },
-    increment: function (event) {
-      console.log('Button Pressed. Event DatA: ', event)
-
-      // Increment the count by one
-      this.counterBtn = this.counterBtn + 1
-      var topic = this.msgRecvd.topic || 'uibuilder/vue'
-      uibuilder.send({
-        topic: topic,
-        payload: {
-          type: 'counterBtn',
-          btnCount: this.counterBtn,
-          message: this.inputText,
-          inputChkBox: this.inputChkBox
-        }
-      })
-    }, // --- End of increment --- //
-
-    // return formatted HTML version of JSON object
-    syntaxHighlight: function (json) {
-      json = JSON.stringify(json, undefined, 4)
-      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-      json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number'
-        if (/^"/.test(match)) {
-          if (/:$/.test(match)) {
-            cls = 'key'
-          } else {
-            cls = 'string'
-          }
-        } else if (/true|false/.test(match)) {
-          cls = 'boolean'
-        } else if (/null/.test(match)) {
-          cls = 'null'
-        }
-        return '<span class="' + cls + '">' + match + '</span>'
-      })
-      return json
-    } // --- End of syntaxHighlight --- //
-  }, // --- End of methods --- //
+    }
+  },
 
   // Available hooks: init,mounted,updated,destroyed
   mounted: function () {
@@ -194,48 +102,6 @@ var app1 = new Vue({
         vueApp.timeMessage = msg.payload
       }
     })
-    // As we receive new messages, we get an updated count as well
-    uibuilder.onChange('msgsReceived', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange] Updated count of received msgs:', newVal)
-      vueApp.msgsReceived = newVal
-    })
-
-    // If we receive a control message from Node-RED, we can get the new data here - we pass it to a Vue variable
-    uibuilder.onChange('ctrlMsg', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange:ctrlMsg] CONTROL msg received from Node-RED server:', newVal)
-      vueApp.msgCtrl = newVal
-    })
-    // Updated count of control messages received
-    uibuilder.onChange('msgsCtrl', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange:msgsCtrl] Updated count of received CONTROL msgs:', newVal)
-      vueApp.msgsControl = newVal
-    })
-    // #endregion ---- End of Trace Received Messages ---- //
-
-    // #region ---- Trace Sent Messages ---- //
-    // You probably only need these to help you understand the order of processing //
-    // If a message is sent back to Node-RED, we can grab a copy here if we want to
-    uibuilder.onChange('sentMsg', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange:sentMsg] msg sent to Node-RED server:', newVal)
-      vueApp.msgSent = newVal
-    })
-    // Updated count of sent messages
-    uibuilder.onChange('msgsSent', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange:msgsSent] Updated count of msgs sent:', newVal)
-      vueApp.msgsSent = newVal
-    })
-
-    // If we send a control message to Node-RED, we can get a copy of it here
-    uibuilder.onChange('sentCtrlMsg', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange:sentCtrlMsg] Control message sent to Node-RED server:', newVal)
-      vueApp.msgCtrlSent = newVal
-    })
-    // And we can get an updated count
-    uibuilder.onChange('msgsSentCtrl', function (newVal) {
-      // console.info('[indexjs:uibuilder.onChange:msgsSentCtrl] Updated count of CONTROL msgs sent:', newVal)
-      vueApp.msgsCtrlSent = newVal
-    })
-    // #endregion ---- End of Trace Sent Messages ---- //
 
     // If Socket.IO connects/disconnects, we get true/false here
     uibuilder.onChange('ioConnected', function (newVal) {
@@ -251,9 +117,5 @@ var app1 = new Vue({
       // console.info('[indexjs:uibuilder.onChange:serverTimeOffset] Offset of time between the browser and the server has changed to:', newVal)
       vueApp.serverTimeOffset = newVal
     })
-
-  } // --- End of mounted hook --- //
-
-}) // --- End of app1 --- //
-
-// EOF
+  }
+})
